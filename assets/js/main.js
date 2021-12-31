f539a711e180884edc575aaebc9e6e181ec10670
Jorge Solis Prado <yonodibujo@gmail.com>
2021-12-31 13:10:51 -0300

    Se modifica el color de titulos y logo via JS , se agrega interacciones js jquery, modal de bienvenida , onclick de busqueda en la seccion 'que hacer' y se prepara el documento para el Deploy.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,45 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+//WELLCOME MODAL
+$(document).ready(function(){
+    $(".my__modal").modal('show');
+});
+
+//TEXT DESAPPEAR
+$(document).ready(function(){
+  $(".title-destinos").click(function(){
+    $(".card-text-destinos").fadeToggle("slow", "linear");
+  });
+});
+//CHANGUE COLOR
+function myFunction(element,clr) {
+  element.style.color = clr;
+}
+//SWEET ALERT
+const buscarOpciones = document.getElementById('buscar');
+buscarOpciones.addEventListener('click', function (){
+  let timerInterval
+Swal.fire({
+  title: 'Buscando la mejor opción',
+  html: 'Estamos recorriendo chile <b></b> para encontrar tu mejor opción.',
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+})
