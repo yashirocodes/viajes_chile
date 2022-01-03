ab4a12efb343809bee02ca8790620ab599afff00
Jorge Solis Prado <yonodibujo@gmail.com>
2022-01-03 15:21:50 -0300

    se dio formato al codigo para dejarlo legible y indentado .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,55 @@
 //TOOLTIP
-let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
 
 //WELLCOME MODAL
-$(document).ready(function(){
-    $(".my__modal").modal('show');
+$(document).ready(function () {
+  $(".my__modal").modal("show");
 });
 
 //TEXT DESAPPEAR
-$(document).ready(function(){
-  $(".title-destinos").click(function(){
+$(document).ready(function () {
+  $(".title-destinos").click(function () {
     $(".card-text-destinos").fadeToggle("slow", "linear");
   });
 });
 //CHANGUE COLOR
-function myFunction(element,clr) {
+function myFunction(element, clr) {
   element.style.color = clr;
 }
 //SWEET ALERT
-const buscarOpciones = document.getElementById('buscar');
-buscarOpciones.addEventListener('click', function (){
-  let timerInterval
-Swal.fire({
-  title: 'Buscando la mejor opción',
-  html: 'Estamos recorriendo chile <b></b> para encontrar tu mejor opción.',
-  timer: 3000,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading()
-    const b = Swal.getHtmlContainer().querySelector('b')
-    timerInterval = setInterval(() => {
-      b.textContent = Swal.getTimerLeft()
-    }, 100)
-  },
-  willClose: () => {
-    clearInterval(timerInterval)
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    console.log('I was closed by the timer')
-  }
-})
-})
+const buscarOpciones = document.getElementById("buscar");
+buscarOpciones.addEventListener("click", function () {
+  let timerInterval;
+  Swal.fire({
+    title: "Buscando la mejor opción",
+    html: "Estamos recorriendo chile <b></b> para encontrar tu mejor opción.",
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 100);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+    }
+  });
+});
 // nav scroll
 
 const nav = document.querySelector(".navbar");
-window.addEventListener("scroll", () =>{
+window.addEventListener("scroll", () => {
   nav.classList.toggle("change", window.scrollY > 0);
-})
+});
